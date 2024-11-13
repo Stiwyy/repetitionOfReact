6a1fa278b6bc08d2ca008820e5d5633ac39e6921
Stiwyy <andrin.renggli@lernende.bbw.ch>
2024-11-13 16:07:09 +0100

    made a const for the map functions

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ import Table from './Table';
 
 function App() {
     const modelsHeader = ['id', 'model', 'type'];
-    const modelsBody = [
-        [1, 'Corsa', 'small'],
-        [2, 'Astra', 'family'],
-        [3, 'Mokka', 'SUV'],
-        [4, 'Movano', 'transporter']
-    ];
+    const modelsBody = [[1, 'Corsa', 'small'], [2, 'Astra', 'family'], [3, 'Mokka', 'SUV'], [4, 'Movano', 'transporter']];
 
     return (
-        <div className="App">
-            <h1>Modelle Tabelle</h1>
-            <Table headers={modelsHeader} data={modelsBody} />
-        </div>
-    );
+        <>
+            <div className="App">
+                <h1>Modelle Tabelle</h1>
+                <Table
+                    headers={modelsHeader}
+                    data={modelsBody}
+                />
+            </div>);
+        </>
+    )
 }
 
 export default App;
--- src/Table.jsx
+++ src/Table.jsx
@@ -1,23 +1,28 @@
 import './mvp.css';
 
 function Table({ headers, data }) {
+    // Konstanten für die Mappings erstellen
+    const headerCells = headers.map((header, index) => (
+        <th key={index}>{header}</th>
+    ));
+
+    const dataRows = data.map((row, rowIndex) => (
+        <tr key={rowIndex}>
+            {row.map((value, valueIndex) => (
+                <td key={valueIndex}>{value}</td>
+            ))}
+        </tr>
+    ));
+
     return (
         <table border="1">
             <thead>
             <tr>
-                {headers.map((header, index) => (
-                    <th key={index}>{header}</th>
-                ))}
+                {headerCells}
             </tr>
             </thead>
             <tbody>
-            {data.map((row, rowIndex) => (
-                <tr key={rowIndex}>
-                    {row.map((value, valueIndex) => (
-                        <td key={valueIndex}>{value}</td>
-                    ))}
-                </tr>
-            ))}
+            {dataRows}
             </tbody>
         </table>
     );
